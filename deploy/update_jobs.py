72c265c2e433301dfb9377424a3399eefb2862f7
sebastian <sebastianoliveros2605@gmail.com>
2025-05-29 12:41:17 -0500

    Agregando modulos para el Job parse_headlines

--- deploy/update_jobs.py
+++ deploy/update_jobs.py
@@ -6,23 +6,32 @@ glue = boto3.client('glue')
 role_arn = 'arn:aws:iam::302119430275:role/LabRole'
 s3_bucket_scripts = 's3://guardar-html/scripts-glue/' # Definir el bucket base para los scripts
 
-# Actualizar download_headlines a Python Shell
+modules_to_install = [
+    "beautifulsoup4",
+    "requests"
+]
+
+# Actualizar parse_headlines a Python Shell con módulos adicionales vía pip
 try:
     glue.update_job(
-        JobName='download_headlines',
+        JobName='parse_headlines',
         JobUpdate={
             'Role': role_arn,
             'Command': {
-                'Name': 'pythonshell', # <--- CAMBIO CLAVE
-                'ScriptLocation': f'{s3_bucket_scripts}download_headlines.py',
-                'PythonVersion': '3'   # Especifica la versión de Python
+                'Name': 'pythonshell',
+                'ScriptLocation': f'{s3_bucket_scripts}parse_headlines.py',
+                'PythonVersion': '3' # Asegúrate que coincida con la versión para la que existen los módulos
             },
-
+            'DefaultArguments': { # Job Parameters
+                '--additional-python-modules': ",".join(modules_to_install) # Lista separada por comas
+                # '--enable-auto-scaling': 'true', # Ejemplo de otro parámetro si es necesario
+            },
         }
     )
-    print("Job 'download_headlines' actualizado a Python Shell exitosamente.")
+    print("Job 'parse_headlines' actualizado a Python Shell con módulos adicionales (pip) exitosamente.")
 except Exception as e:
-    print(f"Error actualizando 'download_headlines': {e}")
+    print(f"Error actualizando 'parse_headlines': {e}")
+
 
 # Actualizar parse_headlines a Python Shell
 try:
